655c561666e022c7b7cc182c444c669e63ab23f0
David Boxwell <34252724+skinzdb@users.noreply.github.com>
2023-05-08 19:32:14 +0100

    Update Components Transform default constructor

--- kuai/src/kuai/Components/Components.h
+++ kuai/src/kuai/Components/Components.h
@@ -46,7 +46,7 @@ namespace kuai {
 	class Transform : public Component
 	{
 	public:
-		Transform() : pos(0,0,0) {}
+		Transform() = default;
 		Transform(const glm::vec3& pos) : pos(pos) {}
 
 		glm::vec3& getPos() { return pos; }
